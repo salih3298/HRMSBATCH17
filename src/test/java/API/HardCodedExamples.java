86bb788d0d9380f62f0397391bb7f314e8158448
salih3298 <salih3298@gmail.com>
2023-12-02 19:12:23 +0300

    cucumber

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -5,18 +5,21 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.runner.Request;
+import org.junit.Test;
 
 
 public class HardCodedExamples {
 
     // in rest assured base uri = base URL
     String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
-    String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDEzMDU0NjMsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTcwMTM0ODY2MywidXNlcklkIjoiNjA4MCJ9.D77mrc9pM7yfZOTBfumixdKERw_mHxQIUmuuMcQo4zw";
+    String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDE1MzIwNTIsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTcwMTU3NTI1MiwidXNlcklkIjoiNjA4MCJ9.1xyAXRjk-UyMRqXC_7vSjkaHM-BXkGAGs0Y8AYQD4CA";
 
+    @Test
     public void createEmployee() {
         // prepare the request
+        //request specification allows us to prepare the request and gives it in variable
         RequestSpecification request = given().header("Content-Type", "application/json")
                 .header("Authorization", token)
                 .body("{\n" +
@@ -28,13 +31,19 @@ public class HardCodedExamples {
                         "  \"emp_status\": \"confirmed\",\n" +
                         "  \"emp_job_title\": \"QA\"\n" +
                         "}");
-        // send the request
+        //send the request for creating the employee
         // response is the class which holds the complete response body and the info
         Response response = request.when().post("/createEmployee.php");
 
-        // validate the response
+        // to print the response in console
+        response.prettyPrint();
+
+        // validate the response status
         response.then().assertThat().statusCode(201);
+        // validate the body
+        response.then().assertThat().body("Message", equalTo("Employee Created"));
+        response.then().assertThat().body("Employee.emp_firstname", equalTo("Salih"));
+        response.then().assertThat().header("Connection", equalTo("Keep-Alive"));
+
     }
-
-
 }
